181d03d8a694a13cf2d77ca9758f5498b4a0f555
Sheena Artrip <sheena.artrip@gmail.com>
2016-09-01 20:02:03 -0400

    make id omitempty

--- src/microblag/user.go
+++ src/microblag/user.go
@@ -4,7 +4,7 @@ import "time"
 
 // A User is a user of a system
 type User struct {
-	ID          string    `json:"id"`
+	ID          string    `json:"id,omitempty"`
 	Username    string    `json:"username"`
 	DisplayName *string   `json:"display_name"`
 	Created     time.Time `json:"created"`
